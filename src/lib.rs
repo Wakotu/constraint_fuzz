d9d3b289cc3eb99fb528a3f245961704e29933e3
Wakotu <442444286@qq.com>
2025-03-22 16:24:16 +0800

    customize logging format

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod analysis;
+use colored::*;
 pub mod ast;
 pub mod config;
 pub mod deopt;
@@ -38,8 +39,32 @@ pub fn init_logger() -> Result<()> {
     Ok(())
 }
 
+fn my_format(
+    write: &mut dyn std::io::Write,
+    now: &mut flexi_logger::DeferredNow,
+    record: &log::Record,
+) -> std::io::Result<()> {
+    let level = match record.level() {
+        log::Level::Error => "ERROR".red().bold(),
+        log::Level::Warn => "WARN".yellow().bold(),
+        log::Level::Info => "INFO".green().bold(),
+        log::Level::Debug => "DEBUG".blue().bold(),
+        log::Level::Trace => "TRACE".purple().bold(),
+    };
+    write!(
+        write,
+        "[{}] {} - {}",
+        now.now().format("%Y-%m-%d %H:%M:%S"),
+        level,
+        record.args()
+    )?;
+    Ok(())
+}
+
 pub fn init_debug_logger() -> Result<()> {
-    flexi_logger::Logger::try_with_env_or_str("debug")?.start()?;
+    flexi_logger::Logger::try_with_env_or_str("debug")?
+        .format(my_format)
+        .start()?;
     Ok(())
 }
 
